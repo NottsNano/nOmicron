c70fddb487e63c28783e324bee8530b47a51ce6a
Oliver Gordon <oliver.gordon@nottingham.ac.uk>
2019-08-22 11:39:21 +0100

    Add tip scratch

--- nOmicron/microscope/conditioning.py
+++ nOmicron/microscope/conditioning.py
@@ -65,7 +65,7 @@ def tip_crash(delta_z, pos=(-1, -1), delay=0, slew_rate=None):
     Parameters
     ----------
     delta_z : float
-        The depth to ramp in meters
+        The depth to ramp in meters.
     pos : tuple
         The position in normalised co-ordinate range(-1, 1) to perform the crash at.
         Default is (-1, -1), which corresponds to the bottom left corner of the scan window
@@ -76,6 +76,7 @@ def tip_crash(delta_z, pos=(-1, -1), delay=0, slew_rate=None):
         The slew rate in metres/second. Not enabled in None (default)
 
     """
+    assert delta_z > 0, "delta_z must be positive"
     mo.xy_scanner.Execute_Port_Colour("ZRamp")
     mo.regulator.Enable_Z_Ramp_Slew_Rate(False)
     mo.regulator.Z_Ramp_Delay(delay)
@@ -83,7 +84,7 @@ def tip_crash(delta_z, pos=(-1, -1), delay=0, slew_rate=None):
     if slew_rate is not None:
         mo.regulator.Enable_Z_Ramp_Slew_Rate(True)
         mo.regulator.Z_Ramp_Slew_Rate(slew_rate)
-    mo.regulator.Z_Ramp(delta_z)
+    mo.regulator.Z_Ramp(-delta_z)
 
     mo.experiment.pause()
     mo.xy_scanner.Execute_Port_Colour("ZRamp")
@@ -100,13 +101,37 @@ def tip_crash(delta_z, pos=(-1, -1), delay=0, slew_rate=None):
     mo.experiment.resume()
 
 
-def tip_scratch(delta_z, start_pos=(), end_pos=()):
-    # move to position
+def tip_scratch(delta_z, end_pos, start_pos=None):
+    """
+    Super destructive. Press the tip into the surface, move it, then come back out.
+
+    Attributes
+    ----------
+    delta_z : float
+        The depth to ramp in meters.
+    start_pos : tuple or None
+        The position in normalised co-ordinate range(-1, 1) to perform the initial crash at. Set to None (default)
+        to start from the current position of the tip
+    end_pos : tuple or None
+        The position in normalised co-ordinate range(-1, 1) to finish the scratch at.
+    """
+
     mo.xy_scanner.Store_Current_Position(True)
-    # press in the tip
-    mo.regulator.Z_Offset()
-    # move the tip
-    # unpress the tip
-    # return to start position
+    mo.regulator.Feedback_Loop_Enabled(False)
+
+    # Go to start position
+    if start_pos is not None:
+        mo.xy_scanner.Target_Position(start_pos)
+        mo.xy_scanner.move()
+
+    mo.regulator.Z_Offset(delta_z)  # Press in the tip
+
+    mo.xy_scanner.Target_Position(end_pos)  # Move the tip
+    mo.xy_scanner.move()
+
+    mo.regulator.Z_Offset(0)  # Un-press the tip
+    mo.regulator.Feedback_Loop_Enabled(True)
+
+    # Return to start position
     mo.xy_scanner.Return_To_Stored_Position(True)
     mo.xy_scanner.Store_Current_Position(False)
